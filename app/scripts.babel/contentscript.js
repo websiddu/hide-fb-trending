e5c12edebe32c5f69b001c4d93b7880140efcb23
Siddhartha <gsid@google.com>
2016-11-13 23:17:36 -0800

    Adding content scirpt

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+Element.prototype.remove = function() {
+  this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+  for (var i = this.length - 1; i >= 0; i--) {
+    if (this[i] && this[i].parentElement) {
+      this[i].parentElement.removeChild(this[i]);
+    }
+  }
+}
+
+var div = document.getElementById('pagelet_trending_tags_and_topics')
+div.remove();
